d80d9970d1fa8b89b0e6b4226c22d940469b3d1b
Jye Harry <jye.harry@nine.com.au>
2024-05-07 15:24:49 +1000

    Update setup script

--- .setup.sh
+++ .setup.sh
@@ -1,16 +1,24 @@
 #! /bin/zsh
 
-brew install ripgrep
-brew install fd
-brew install fzf
-brew install zsh-autosuggestions
-brew install font-hack-nerd-font
-brew install bun
-brew install tree
-brew install zsh-syntax-highlighting
-brew install sd
-brew install zoxide
-brew install obsidian
-brew install gh
+git --git-dir=$HOME/.dotfiles --work-tree=$HOME config --local status.showUntrackedFiles no || { echo 'Failed to set git config'; exit 1; }
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+brew install neovim ripgrep fd fzf zsh-autosuggestions font-hack-nerd-font bun tree zsh-syntax-highlighting sd zoxide obsidian gh go python3 || { echo 'Failed brew installing something'; exit 1; }
+
+# installs NVM (Node Version Manager)
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | zsh || { echo 'Failed installing nvm'; exit 1; }
+
+# download and install Node.js
+nvm install 22 || { echo 'Failed nvm installing node'; exit 1; }
+
+# verifies the right Node.js version is in the environment
+node -v # should print `v22.1.0`
+
+# verifies the right NPM version is in the environment
+npm -v # should print `10.7.0`
+
+curl -L https://iterm2.com/downloads/stable/latest --output ~/Downloads/iTerm.zip || { echo 'Failed downloading iTerm2 with curl'; exit 1; }
+unzip ~/Downloads/iTerm.zip -d ~/Applications/ || { echo 'Failed unzipping and installing iTerm2'; exit 1; }
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" || { echo 'Failed installing omz'; exit 1; }
+
+echo "Don't forget to set font-hack-nerd-font in terminal preferences"
