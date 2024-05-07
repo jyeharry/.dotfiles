b4ccccfbfb1e487c590148f0b724f796a814053e
Jye Harry <jye.harry@nine.com.au>
2024-05-07 14:47:35 +1000

    Add setup script

---
+++ .setup.sh
@@ -0,0 +1,15 @@
+#! /bin/zsh
+
+brew install ripgrep
+brew install fd
+brew install fzf
+brew install zsh-autosuggestions
+brew install font-hack-nerd-font
+brew install bun
+brew install tree
+brew install zsh-syntax-highlighting
+brew install sd
+brew install zoxide
+brew install obsidian
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
