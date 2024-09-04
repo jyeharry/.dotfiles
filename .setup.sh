bcc1193900d89097f75f26b587b390f8eddb4c22
Jye Harry <jye.harry@nine.com.au>
2024-09-05 00:08:28 +1000

    Update setup script

--- .setup.sh
+++ .setup.sh
@@ -3,38 +3,42 @@
 git --git-dir=$HOME/.dotfiles --work-tree=$HOME config --local status.showUntrackedFiles no || { echo 'Failed to set git config'; exit 1; }
 
 if ! command -v brew &> /dev/null; then
-  echo "Installing homebrew"
-  /bin/zsh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" || { echo 'Failed to install homebrew'; exit 1; }
+  echo "\nInstalling homebrew"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" || { echo 'Failed to install homebrew'; exit 1; }
+  eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
-brew install git neovim ripgrep fd fzf zsh-autosuggestions pngpaste font-hack-nerd-font bun tree zsh-syntax-highlighting sd zoxide obsidian gh go python3 rlwrap swiftlint swift-format composer && brew install --cask nikitabobko/tap/aerospace || { echo 'Failed brew-installing something'; exit 1; }
+brew install git neovim ripgrep fd fzf zsh-autosuggestions pngpaste font-hack-nerd-font oven-sh/bun/bun tree zsh-syntax-highlighting sd zoxide obsidian gh go python3 rlwrap swiftlint swift-format composer && brew install --cask nikitabobko/tap/aerospace || { echo 'Failed brew-installing something'; exit 1; }
 
 # installs NVM (Node Version Manager)
-if ! command -v nvm &> /dev/null; then
-  echo "Installing nvm"
-  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | zsh || { echo 'Failed installing nvm'; exit 1; }
+if [ ! -d "$HOME/.nvm" ]; then
+  echo "\nInstalling nvm"
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash || { echo 'Failed installing nvm'; exit 1; }
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 fi
 
 # download and install Node.js
 if ! command -v node &> /dev/null; then
-  echo "Installing node"
+  echo "\nInstalling node"
   nvm install 22 || { echo 'Failed nvm installing node'; exit 1; }
 fi
 
-if ! [[ -d "/Applications/iTerm.app" ]]; then
-  echo "Installing iTerm2"
+if [[ -d "/Applications/iTerm.app" ]]; then
+  echo "\nInstalling iTerm2"
   curl -L https://iterm2.com/downloads/stable/latest --output ~/Downloads/iTerm.zip || { echo 'Failed downloading iTerm2 with curl'; exit 1; }
   unzip ~/Downloads/iTerm.zip -d ~/Applications/ || { echo 'Failed unzipping and installing iTerm2'; exit 1; }
 fi
 
-if ! command -v omz &> /dev/null; then
-  echo "Installing ohmyzsh"
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  echo "\nInstalling ohmyzsh"
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" || { echo 'Failed installing omz'; exit 1; }
   rm ~/.zshrc
 fi
 
-git --git-dir=$HOME/.dotfiles --work-tree=$HOME checkout
+git --git-dir=$HOME/.dotfiles --work-tree=$HOME checkout .zshrc
 
-omz reload
+source ~/.zshrc
 
-echo "Don't forget to set Hack Nerd Font Mono in terminal preferences"
+echo "\nDon't forget to set Hack Nerd Font Mono in terminal preferences"
